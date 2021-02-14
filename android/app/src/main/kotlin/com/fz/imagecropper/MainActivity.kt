4fab8f5ff867ce0fd374a3f3c9b61d682134a380
FrazileInc <frazile.com@gmail.com>
2021-02-14 10:12:04 +0530

    String printed from Kotlin Native

--- android/app/src/main/kotlin/com/fz/imagecropper/MainActivity.kt
+++ android/app/src/main/kotlin/com/fz/imagecropper/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.fz.imagecropper
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.fz.imagecropper/image";
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        
+        MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler{ methodcall, result ->
+            if(methodcall.method == "GetName") {
+                val greetings = helloFromNativeCode()
+                result.success(greetings)
+            }
+        }
+    }
+    private fun helloFromNativeCode(): String {
+    return "Hello from Kotlin"
+  }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,6 +7,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static const platform = const MethodChannel('com.fz.imagecropper/image');
+
+  @override
+  void initState() {
+    PrintName();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,5 +27,15 @@ class _HomePageState extends State<HomePage> {
         child: Text('NAME'),
       ),
     );
+  }
+
+  void PrintName() async {
+    String value = '';
+    try {
+      value = await platform.invokeMethod("GetName");
+    } catch (e) {
+      print(e);
+    }
+    print(value);
   }
 }
